ada027fedb3409f7eab668a81eb3ae55efce7cce
fmb21117 <angus.duffy.2021@uni.strath.ac.uk>
2024-05-15 11:44:38 +0100

    feat: divison and multiplication features done

--- js/test_money.js
+++ js/test_money.js
@@ -1,14 +1,28 @@
 const assert = require("assert");
 
-class Dollar {
-  constructor(amount) {
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
+
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
+  }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
   }
 }
 
-let fiver = new Dollar(5);
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
+
+let fiveEuro = new Money(5, "EUR");
+let tenEuro = new Money(10, "EUR");
+assert.deepStrictEqual(fiveEuro.times(2), tenEuro);
+
+let originalMoney = new Money(10, "USD");
+let expectedMoney = new Money(5, "USD");
+assert.deepStrictEqual(originalMoney.divide(2), expectedMoney);
